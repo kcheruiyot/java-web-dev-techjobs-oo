dc78f211cddef76c37c33bcef017706935548a2d
kcheruiyot <57727200+kcheruiyot@users.noreply.github.com>
2020-02-05 13:53:24 -0600

    Refactored Classes

--- src/org/launchcode/techjobs_oo/CoreCompetency.java
+++ src/org/launchcode/techjobs_oo/CoreCompetency.java
@@ -6,12 +6,4 @@ public class CoreCompetency extends JobField{
     public CoreCompetency(String value){
         super(value);
     }
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof CoreCompetency)) return false;
-        CoreCompetency that = (CoreCompetency) o;
-        return this.getId() == that.getId();
-    }
-
 }
--- src/org/launchcode/techjobs_oo/PositionType.java
+++ src/org/launchcode/techjobs_oo/PositionType.java
@@ -3,15 +3,7 @@ package org.launchcode.techjobs_oo;
 import java.util.Objects;
 
 public class PositionType extends JobField{
-
     public PositionType(String value){
         super(value);
-    }
-    @Override
-    public boolean equals(Object o){
-        if(this==o) return true;
-        if(!(o instanceof PositionType))return  false;
-        PositionType positionType = (PositionType) o;
-        return getId()==positionType.getId();
     }
 }
